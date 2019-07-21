6a9c2a8e8ddf84bc8198fd2062143a759c0a2b14
Shane Johnson <shane@moonwaveinteractive.com>
2019-07-21 05:04:32 -0400

    Add files via upload

---
+++ TokenMarcherAlgorithm.cs
@@ -0,0 +1,25 @@
+﻿// (c) 2019 Moonwave Interactive. All rights reserved under MIT License.
+
+/// <summary>
+/// Returns a <see cref="System.Collections.Generic.List{T}"/> of tokens from the specified text string.
+/// </summary>
+/// <param name="text">The text from which to tokenize.</param>
+/// <returns>A <see cref="System.Collections.Generic.List{T}"/> of tokens from the specified text string.</returns>
+private static List<string> Tokenize(string text)
+{
+    List<string> tokens = new List<string>();
+    List<char> chars = text.ToList();
+    StringBuilder buffer = new StringBuilder();
+    for (int i = 0; i < chars.Count; i++)
+    {
+        if (char.IsLetterOrDigit(chars[i])) { buffer.Append(chars[i]); }
+        else
+        {
+            if (buffer.Length != 0) { tokens.Add(buffer.ToString()); buffer.Clear(); }
+            // Here you can define a custom pattern using a character array.
+            if (chars[i] != ' ') { tokens.Add(chars[i].ToString()); }
+        }
+    }
+    if (buffer.Length != 0) { tokens.Add(buffer.ToString()); }
+    return tokens;
+}
